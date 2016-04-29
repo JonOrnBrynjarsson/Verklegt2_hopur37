0a14e5b7c16f9929e3c5d4a291dc75ed4da7e6a4
JonOrnBrynjarsson <jonoganna@hotmail.com>
2016-04-29 10:50:35 +0000

    Grunnur að verkefni
    Henti inn grunnuppsetningu í VS með Unit testi og individual
    authentication.

---
+++ Mooshak20/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mooshak20.MvcApplication" Language="C#" %>
---
+++ Mooshak20/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mooshak20.Startup))]
+namespace Mooshak20
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
